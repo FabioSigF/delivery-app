a6e46ddea99970c85707d256ce3d97e070c677da
FelipeSignoriini <95833161+FabioSigF@users.noreply.github.com>
2024-07-08 21:24:09 -0300

    initial style configs

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,12 +1,32 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+// Estado reativo para armazenar a URL do logo
+const logoURL = ref('');
+
+// Função assíncrona para buscar a URL do logo
+const getLogo = async () => {
+  const data = await $fetch('/api/db');
+
+  if (data) {
+    console.log(data.company.logo);
+    logoURL.value = data.company.logo;
+  } else {
+    console.error('Failed to fetch logo:', error.value);
+  }
+};
+
+// Chamar a função `getLogo` quando o componente for montado
+onMounted(() => {
+  getLogo();
+});
+</script>
+
 <template>
   <div>
-    <img src="/assets/images/logo.png" alt="" />
+    <img :src="logoURL" alt="Company Logo" />
   </div>
 </template>
 
-<script>
-</script>
-
-<style lang="scss" scoped>
-
+<style scoped>
 </style>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-
+  <div class="container flex flex-col">
+    <div class="flex gap-3 items-center w-full">
+      <div class="px-3 py-2 border">
+        <Logo />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Logo from '../../components/Logo.vue';
+
   export default {
     
   }
